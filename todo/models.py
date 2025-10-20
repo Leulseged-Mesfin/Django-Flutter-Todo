03eb41010f895b4f3af1e5f8f9834333eef58b5d
Leulseged-Mesfin <leulsegedmesfin16@gmail.com>
2025-10-20 11:24:02 +0300

    First Commit

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
